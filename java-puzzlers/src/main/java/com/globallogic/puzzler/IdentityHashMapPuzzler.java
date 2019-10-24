36d57c98d5cdf41d5ce6cad35eba42f65922941f
Bohdan Vanchuhov <bohdan.vanchuhov@globallogic.com>
2019-10-24 12:24:05 +0300

    add IdentityHashMapPuzzler

---
+++ java-puzzlers/src/main/java/com/globallogic/puzzler/IdentityHashMapPuzzler.java
@@ -0,0 +1,18 @@
+package com.globallogic.puzzler;
+
+import java.util.IdentityHashMap;
+
+public class IdentityHashMapPuzzler {
+
+    public static void main(String[] args) {
+        var map = new IdentityHashMap<Object, String>();
+        map.put(100, "C++");
+        map.put(100, "Java");
+        map.put(200, "Python");
+        map.put(200, "Kotlin");
+        map.put("lang", "Groovy");
+        map.put("lang", "Golang");
+
+        System.out.println(map.values());
+    }
+}
