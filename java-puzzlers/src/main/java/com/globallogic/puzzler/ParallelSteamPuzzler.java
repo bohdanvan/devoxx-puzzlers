958789bf54c34080cfd1ffbabe75dde7f79c3bd3
Bohdan Vanchuhov <bohdan.vanchuhov@globallogic.com>
2019-10-31 22:33:06 +0200

    fix ParallelSteamPuzzler

--- java-puzzlers/src/main/java/com/globallogic/puzzler/ParallelSteamPuzzler.java
+++ java-puzzlers/src/main/java/com/globallogic/puzzler/ParallelSteamPuzzler.java
@@ -2,7 +2,7 @@ package com.globallogic.puzzler;
 
 import java.util.stream.Stream;
 
-public class ParallelSteamPuzzler { // Medium, Theoretical
+public class ParallelSteamPuzzler { // Medium, Theoretical/Practical
 
     public static void main(String[] args) {
         var res = Stream.of("8", "11", "13").parallel().reduce("Java ", String::concat);
